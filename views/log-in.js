03d744f8c6405c8de10d0da9221b912b0aa8f210
Danica Lantin <danica.lantin13@gmail.com>
2020-08-23 10:55:44 +1200

    front end of log-in page

---
+++ views/log-in.js
@@ -0,0 +1,9 @@
+
+function validateForm() {
+    var username ="123@gmail.com"
+    var password ="hello"
+    if (username == "") {
+        alert("Insert email and password");
+        return false;
+    }
+}
