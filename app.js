0d43edc7dc706127c5e4998c76c95bb4545f9bc5
Logan <logan.wm.brantley@gmail.com>
2020-08-23 11:26:48 +1200

    Small changes to html

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+
+app.use(express.static("css"));
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', function (req, res) {
-    res.send('Testing Pay Online Branch!');
+    res.render('log-in.html');
 });
+
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('App listening on port 3000!');
 });
