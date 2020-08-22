599f9974f601c45856905fa8c9df5f00c997360c
Logan <logan.wm.brantley@gmail.com>
2020-08-22 16:46:12 +1200

    Fixed app.js code to display html files

--- app.js
+++ app.js
@@ -4,12 +4,20 @@
 
 
 var express = require('express');
-const { request } = require('express');
 var app = express();
-app.get('/', function (req, res) {
-    //res.send('Testing Pay Online Branch!'); 
-    res.render("Pay-Online.html")
+var bodyParser = require('body-parser');
+
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res){
+    res.render('Pay-Online.html');
 });
+
+// app.get('/', function (req, res) {
+//     //res.send('Testing Pay Online Branch!');
+//     res.render("Pay-Online.html")
+// });
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
